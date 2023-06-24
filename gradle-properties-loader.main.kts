3e662e0034b7d3531ec32f7c06ac243183517f06
MORIMORI0317 <morimori.0317@outlook.jp>
2023-06-24 22:50:25 +0900

    nna

--- gradle-properties-loader.main.kts
+++ gradle-properties-loader.main.kts
@@ -5,9 +5,10 @@
  * @author MORIMORI0317
  */
 
+import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
-import java.util.HashMap
+import java.util.stream.Collectors
 
 
 /**
@@ -24,17 +25,11 @@ val gpPath: Path = wrkDir.resolve("gradle.properties")
  * "gradle.properties"を読み込んで取得する
  */
 fun getGradleProperties(): Map<String, String> {
-    /*return gpPath
+    return gpPath
         .let { Files.lines(it) }
         .filter { it.isNotBlank() }
         .filter { !it.trim().startsWith("#") }
         .map { it.split("=") }
-        .collect(Collectors.toMap({ it[0].trim() }, { it[1].trim() }))*/
-
-    val map = HashMap<String, String>()
-
-    map.put("課長壊れる", "野獣先輩")
-
-    return map;
+        .collect(Collectors.toMap({ it[0].trim() }, { it[1].trim() }))
 }
 
