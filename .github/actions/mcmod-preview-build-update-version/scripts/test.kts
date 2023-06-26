9591cd3e9b8f573ae561716ed0a7acfd1ba6d420
MORIMORI0317 <morimori.0317@outlook.jp>
2023-06-26 23:04:12 +0900

    nna

--- .github/actions/mcmod-preview-build-update-version/scripts/test.kts
+++ .github/actions/mcmod-preview-build-update-version/scripts/test.kts
@@ -1,3 +1,8 @@
 #!/usr/bin/env kotlin
 
+@file:Import("../../../../gradle-properties-loader.main.kts")
+
+val modver=getModVersion()
+
+println(modver)
 println("ｳｧｧ!!ｵﾚﾓｲｯﾁｬｳｩｩｩ!!!ｳｳｳｳｳｳｳｳｳｩｩｩｩｩｩｩｩｳｳｳｳｳｳｳｳ!ｲｨｨｲｨｨｨｲｲｲｨｲｲｲｲ")
